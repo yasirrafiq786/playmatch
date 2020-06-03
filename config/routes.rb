e08321a31dd09fa5d197ddb3b8da5f6683535cbd
Yasir Malik <yasirrafiq786@gmail.com>
2020-06-03 20:34:17 +1000

    Added a navigation page and custome route for it IFF development mode

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'availabilities/index'
-  get 'availabilities/new'
-  get 'availabilities/create'
-  get 'availabilities/show'
   devise_for :users
   root to: 'pages#home'
 
   resources :sports, only: [:index, :show]
   resources :availabilities , only:[:index, :new, :create, :show]
+
+  if Rails.env.development?
+    get '/navigation', to: 'pages#navigation'
+  end
 end
