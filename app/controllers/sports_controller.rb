6db749c7c0ec7f3cced95bf2db68f38e4038c7af
Sonya Duke <sonya.j.duke@gmail.com>
2020-06-04 20:56:05 +1000

    Added show page to sports, showing profiles of players

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -2,4 +2,9 @@ class SportsController < ApplicationController
   def index
     @sports = Sport.all
   end
+
+  def show
+    # @sports = Sport.where("celebrity_id = ?", params[:id])
+    @sport = Sport.find(params[:id])
+  end
 end
