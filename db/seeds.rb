0107334158cd126ca86d839cb2aa656b7f30d3eb
Andy Leong <mindreko@gmail.com>
2020-06-09 20:56:32 +1000

    basic search functionality working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+puts "Standby"
 yasir = User.create(first_name:"Yasir", email:"yasir@yasir.com", password:"123abc")
 andy = User.create(first_name:"Andy", email:"andy@andy.com", password:"123abc")
 squash = Sport.create(name:"Squash")
@@ -5,7 +6,7 @@ badminton = Sport.create(name:"Badminton")
 football = Sport.create(name:"Football")
 frisbee = Sport.create(name:"Frisbee")
 yasir.sports = [squash, frisbee]
-andy.sports = [badmintion, football]
+andy.sports = [badminton, football]
 yasir_availability = Availability.create(start_time:Date.today, end_time:Date.today+1, sport: squash)
 andy_availability = Availability.create(start_time:Date.today, end_time:Date.today+2, sport: badminton)
 booking1 = Booking.create(sport: squash, status: 'pending', date: Date.today)
@@ -13,3 +14,4 @@ booking1.users = [yasir, andy]
 booking2 = Booking.create(sport: badminton, status: 'pending', date: Date.today+1)
 booking2.users = [andy, yasir]
 review1 = Review.create()
+puts "Complete"
