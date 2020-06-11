a04c17728afa2c60f9a6caf22fc7083f41a95400
Yasir Malik <yasirrafiq786@gmail.com>
2020-06-11 20:19:03 +1000

    Removed Glide

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,13 +29,13 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 // import { initFlatpickr } from '../plugins/init_flatpickr';
-import { initGlide } from '../plugins/init_glide';
+// import { initGlide } from '../plugins/init_glide';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   // initFlatpickr();
-  initGlide();
+  // initGlide();
   
   
 });
