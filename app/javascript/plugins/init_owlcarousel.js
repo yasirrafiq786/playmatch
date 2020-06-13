472e268d5a43cd8efcb742d4275ad3240b3a0b11
Yasir Malik <yasirrafiq786@gmail.com>
2020-06-13 15:35:11 +1000

    x

--- app/javascript/plugins/init_owlcarousel.js
+++ app/javascript/plugins/init_owlcarousel.js
@@ -1,6 +1,6 @@
 import * as $ from 'jquery';
 import 'owl.carousel.es6'
-import "owl.carousel.es6/dist/assets/owl.carousel.css";
+
 
 const initOwlCarousel = () => {
     $('.owl-carousel').owlCarousel({
