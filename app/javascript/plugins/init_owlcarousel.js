9742296580cd970c2481f6b6a53a04bc5a045a93
Yasir Malik <yasirrafiq786@gmail.com>
2020-06-13 12:13:14 +1000

    made owl carousel work

--- app/javascript/plugins/init_owlcarousel.js
+++ app/javascript/plugins/init_owlcarousel.js
@@ -1,10 +1,10 @@
 import * as $ from 'jquery';
 import 'owl.carousel.es6'
- 
+
 const initOwlCarousel = () => {
     $('.owl-carousel').owlCarousel({
         items:1,
-        nav: true
+        
     });
 };
 
