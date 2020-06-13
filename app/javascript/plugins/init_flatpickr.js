d344f2f3ac64740cf0769c46783ba6e68f8a529d
Yasir Malik <yasirrafiq786@gmail.com>
2020-06-13 22:15:49 +1000

    included css inside js files

--- app/javascript/plugins/init_flatpickr.js
+++ app/javascript/plugins/init_flatpickr.js
@@ -1,5 +1,6 @@
 import flatpickr from "flatpickr";
-// import "flatpickr/dist/themes/dark.css";
+import "flatpickr/dist/themes/dark.css";
+
 
 
 const initFlatpickr = () => {
--- app/javascript/plugins/init_owlcarousel.js
+++ app/javascript/plugins/init_owlcarousel.js
@@ -1,10 +1,13 @@
 import * as $ from 'jquery';
-import 'owl.carousel.es6'
+import 'owl.carousel.es6';
+import 'owl.carousel.es6/dist/assets/owl.carousel.css';
 
 
 const initOwlCarousel = () => {
     $('.owl-carousel').owlCarousel({
         items:1,
+        loop: true,
+        dots: false
         
     });
 };
