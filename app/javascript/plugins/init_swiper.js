32f645dab6b6c10200eb07d97ecf567a7db9d285
Yasir Malik <yasirrafiq786@gmail.com>
2020-06-14 14:33:03 +1000

    swiper is working

--- app/javascript/plugins/init_swiper.js
+++ app/javascript/plugins/init_swiper.js
@@ -3,10 +3,16 @@ import 'swiper/css/swiper.css';
 
 const initSwiper = () => { 
     var mySwiper = new Swiper('.swiper-container', {
+        loop: true,
         pagination: {
             el: '.swiper-pagination',
             type: 'bullets',
           },
+        effect: 'coverflow',
+        coverflowEffect: {
+            rotate: 30,
+            slideShadows: false,
+          }
     });
 };
 
