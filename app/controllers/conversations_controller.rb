cfe4bec11559645692eb556df6f71e93ae616557
Yasir Malik <yasirrafiq786@gmail.com>
2020-06-16 10:10:34 +1000

    chat with fixed conversation is working

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,14 +2,8 @@ class ConversationsController < ApplicationController
 
     def show
         @conversation = Conversation.find(params[:id])
+        @message = Message.new
     end
 
-    def create
-        @conversation = Conversation.create
-        if @conversation.create
-            redirect_to
-        else
-            flash[:alert] = "Unable to start conversation!!!"
-        end
-    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   resources :reviews, only: :index
   resources :profiles, only: [ :index, :show ]
 
-  resources :chatrooms, only: [:create, :show] do
+  resources :conversations, only: :show do
     resources :messages, only: :create
   end
 
