fd17f6cf463013a5f6d567b6b207e5cd9d5f781b
Yasir Malik <yasirrafiq786@gmail.com>
2020-06-19 21:48:21 +1000

    corrected address finder function

--- app/javascript/plugins/init_flatpickr.js
+++ app/javascript/plugins/init_flatpickr.js
@@ -7,7 +7,7 @@ const initFlatpickr = () => {
     flatpickr(".flat-pickr",{
         dateFormat: "Y-m-d",
         defaultDate: "today",
-        disableMobile: false
+        disableMobile: true
     });
 }
 
