9147939fd4d14bbf9f04b8d864e3f1e07f777ef5
Yasir Malik <yasirrafiq786@gmail.com>
2020-06-23 17:56:50 +1000

    homepage

--- app/javascript/plugins/init_typed.js
+++ app/javascript/plugins/init_typed.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 const initTyped = () => {
     var typed = new Typed('#typed', 
     {
-        strings: ['<strong>PLAY<u>TENNIS</u></strong><br>','<strong>PLAY<u>SQUASH</u></strong>','<strong>PLAY<u>MATCH</u></strong>'],
+        strings: ['<strong>FEELING <u>BORED</u>?</strong>','<strong>STAY <u>HEALTHY</u>?</strong>','<strong>STAY <u>FIT</u>?</strong>', '<strong>HAVE <u>FUN</u>?</strong>', '<strong>PLAYMATCH</strong>'],
         typeSpeed: 80,
         smartBackspace: true
       });
